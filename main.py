f3eb87abbb4413658eda953649813879a65c6db3
naif779-sys <naif779@gmail.com>
2026-03-03 01:38:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# الإعدادات الخاصة بك
+# إعداداتك الخاصة
 VERIFY_TOKEN = "OverBot_123"
 ACCESS_TOKEN = "EAAV3Lxl8JBIBQ35fnVC1N6BSxSDxPzvqyjFfzmMGGd8IdaDvIUtV0pUZAVJyZCNkMZBbnaB3ycdnrSl1ISfGHsC24zDbAwKgonvUgdrosWZBceWkZAGvwNZAgvRSGZAxDW7F0BJZCYVcbUV5NQ3NGfchdskGygUhC3QWcXtHZBJ1kXeqo2ZC9GeXQofS1HtvB17WYOzt1ZBZBCypzgi6PKxRGAANK2FrqYAaF9Q0tZCLJBqfoZCUNZBAdcSIwBK9nX8ULAk4JfWntCpREZC4TQA5KczUKJIq"
 PHONE_NUMBER_ID = "1025739520622186"
@@ -36,7 +36,5 @@ def send_whatsapp_message(to_number, text):
     requests.post(url, headers=headers, json=payload)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
